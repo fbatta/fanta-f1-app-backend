1c6b466167f795fd3dcf3beb42acfaac008cfa4c
Francesco Battaglini <francesco@battaglini.net>
2026-03-30 15:57:21 +1100

    chore: add clock and timezone beans to app configuration
    - Register `Clock` bean using `Clock.System`
    - Register `TimeZone` bean using `TimeZone.currentSystemDefault()`

--- src/main/kotlin/net/battaglini/fantaf1appbackend/configuration/AppConfiguration.kt
+++ src/main/kotlin/net/battaglini/fantaf1appbackend/configuration/AppConfiguration.kt
@@ -1,7 +1,15 @@
 package net.battaglini.fantaf1appbackend.configuration
 
+import kotlinx.datetime.TimeZone
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import kotlin.time.Clock
 
 @Configuration
 class AppConfiguration {
+    @Bean
+    fun clock(): Clock = Clock.System
+
+    @Bean
+    fun timeZone(): TimeZone = TimeZone.currentSystemDefault()
 }
